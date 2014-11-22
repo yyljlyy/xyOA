c9aaa8f02c8b7972bbd788e3fed3941ab6d1982d
yyljlyy <fkhacker@vip.qq.com>
2014-11-22 12:02:46 +0800

    Role
    Role权限以及hibernate持久层配置

---
+++ src/cn/xxljlxx/xyOA/domain/User.java
@@ -0,0 +1,84 @@
+package cn.xxljlxx.xyOA.domain;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 用户实体
+ * @author lee
+ *
+ */
+public class User {
+	private Long id;
+	private String name;
+	private String loginName;
+	private String passWord;
+	private int gender;
+	private String phone;
+	private String email;
+	private String description;
+	private Department department;
+	private Set<Role> roles = new HashSet<Role>();
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getLoginName() {
+		return loginName;
+	}
+	public void setLoginName(String loginName) {
+		this.loginName = loginName;
+	}
+	public String getPassWord() {
+		return passWord;
+	}
+	public void setPassWord(String passWord) {
+		this.passWord = passWord;
+	}
+	public int getGender() {
+		return gender;
+	}
+	public void setGender(int gender) {
+		this.gender = gender;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Department getDepartment() {
+		return department;
+	}
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+	public Set<Role> getRoles() {
+		return roles;
+	}
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
+	
+}
